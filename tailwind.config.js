9a53523648700a0f0925b5c103ee3048d459e4ce
Fabien Doiron <fabiend.github@gmail.com>
2020-05-26 13:20:04 -0400

    Redesign (#3)
    * In progress
    
    * More progress
    
    * v1

---
+++ tailwind.config.js
@@ -0,0 +1,42 @@
+module.exports = {
+  purge: {
+    content: ["./index.html"],
+  },
+  theme: {
+    fontFamily: {
+      body: [
+        "Work Sans",
+        "-apple-system",
+        "BlinkMacSystemFont",
+        "Segoe UI",
+        "Roboto",
+        "Oxygen",
+        "Ubuntu",
+        "Cantarell",
+        "Fira Sans",
+        "Droid Sans",
+        "Helvetica Neue",
+        "sans-serif",
+      ],
+    },
+    fontSize: {
+      14: ".875rem",
+      16: "1rem",
+      18: "1.125rem",
+      20: "1.25rem",
+      24: "1.5rem",
+      30: "1.875rem",
+      36: "2.25rem",
+      48: "3rem",
+      64: "4rem",
+      80: "5rem",
+      128: "8rem",
+    },
+    colors: {
+      white: "white",
+      black: "black",
+      background: "#f1f3f4",
+      foreground: "#202125",
+    },
+  },
+};
